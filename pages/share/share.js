040a4301b9aaf1b39c91ce95e2ced9b07e6bdd27
sala <1552923147@qq.com>
2020-05-04 21:20:18 +0800

    分享有礼

--- pages/share/share.js
+++ pages/share/share.js
@@ -1,62 +1,43 @@
-// pages/share/share.js
+const util = require('../../utils/util');
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    noData: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  commonList() {
+    util.requests('/business/invite-award/getRecentList', {}).then(res => {
+      this.setData({
+        msgList: res.data.data
+      })
+      wx.setStorageSync('userId', res.data.data.id);
+    })
+  },
+  myList() {
+    util.requests('/business/invite-award/getInviteeList', {}).then(res => {
+      this.setData({
+        myList: res.data.data
+      })
+      if (res.data.data.length>0){
+        this.setData({
+          noData:false
+        })
+      } else {
+        this.setData({
+          noData: true
+        })
+      }
+    })
+  },
   onLoad: function (options) {
     this.setData({
       shareCode: options.shareCode
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    this.commonList()
+    this.myList()
   },
   onShareAppMessage: function (res) {
     var that = this;
